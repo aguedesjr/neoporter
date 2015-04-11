0bada62f2178d97a097849e6df96ac329262f22d
agsoares <agsoares@gmail.com>
2015-04-11 10:02:12 -0300

    Ajuste do logo no rodape para o IE

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div id="tooplate_cr_wrapper">
 	<div id="tooplate_cr">
     	
-        Copyright © 2015 <a href="neoporter.com.br">Neoporter</a> <img src="images/logo.ico" width="18" height="9"> - Todos os direitos reservados.
+        Copyright © 2015 <a href="neoporter.com.br">Neoporter</a> <img src="images/logo.png" width="18" height="9"> - Todos os direitos reservados.
         
     </div> <!-- end of footer wrapper -->
 </div> <!-- end of footer -->
